cc85cd4fb547f9415b2c658030cdff9847648ee2
David J <98175896+jiangd22@users.noreply.github.com>
2023-09-26 23:44:00 -0500

    done with the changes

--- frontend/src/Views/HowToUse.jsx
+++ frontend/src/Views/HowToUse.jsx
@@ -13,11 +13,17 @@ export default function HowToUse() {
           alignItems: "center",
         }}
       >
-        <Paper sx={{ marginTop: 2, padding: 2, margin: "0 auto", maxWidth: "760px" }}>
-          <Typography component="h2" variant="h2" sx={{ textAlign: "center" }}>
-            How to Use Ethical Pipeline
+        {/* <Paper sx={{ p: 4, borderRadius: 2, backgroundColor: "#fff" }}>
+          <Typography component="h1" variant="h1" sx={{ textAlign: "center" }}>
+            How to Use page
           </Typography>
-          {/* <Typography variant="h2">How to Use the AI Chat Bot:</Typography> */}
+        </Paper> */}
+       
+        {/* Card Section */}
+        <Card sx={{ marginTop: 2, padding: 2, margin: "0 auto", maxWidth: "900px" }}>
+        <div style={{ textAlign: 'center' }}>
+          <Typography variant="h2">How to Use the Ethical Pipeline:</Typography>
+        </div>
           <ol>
             <li>
               <Typography variant="body1">
@@ -35,7 +41,7 @@ export default function HowToUse() {
                 <strong>Exploring Further:</strong> You can continue exploring more ethical questions by entering them into the input box and selecting your stance using the buttons. Each time you select a button, you will receive immediate feedback and insights into the collective opinions on that particular question.
               </Typography>
             </li>
-            {/* <li>
+            <li>
               <Typography variant="body1">
                 <strong>About Us:</strong> If you want to learn more about our organization and the purpose of the Ethical Questionnaire, click on the "About Us" section in the navigation bar.
               </Typography>
@@ -44,9 +50,10 @@ export default function HowToUse() {
               <Typography variant="body1">
                 <strong>Returning Home:</strong> At any point during your session, you can click the "Home" button in the navigation bar to return to the home page and start anew.
               </Typography>
-            </li> */}
+            </li>
           </ol>
-        </Paper>
+        </Card>
+
       </Box>
     </Container>
   );
