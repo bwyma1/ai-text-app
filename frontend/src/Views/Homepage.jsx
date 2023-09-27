0f089464c1063d17628dae4953fbfd16119416b7
CarsonRPittman <60508384+CarsonRPittman@users.noreply.github.com>
2023-09-26 23:14:34 -0500

    company name corrections

--- frontend/src/Views/Homepage.jsx
+++ frontend/src/Views/Homepage.jsx
@@ -13,20 +13,26 @@ export default function Homepage() {
         }}
       >
         <Paper sx={{ p: 4, borderRadius: 2, backgroundColor: "#fff" }}>
-          <Typography component="h1" variant="h1" sx={{ textAlign: "center" }}>
-            Welcome to Ethics Pipeline
+          <Typography component="h2" variant="h2" sx={{ textAlign: "center" }}>
+            Welcome to The Crystal Ball of Truth
           </Typography>
           <Typography
             component="p"
             variant="h4"
             sx={{ mt: 2, mx: 10, textAlign: "center" }}
           >
-            The latest AI Ethics Website for all your one-stop online website for answering your moral and ethical
-            questions...
+            The latest AI Ethics Website from Ethical Pipeline 
           </Typography>
           <Typography
             component="p"
             variant="h5"
+            sx={{ mt: 2, mx: 10, textAlign: "center" }}
+          >
+            Your one-stop destination for answering all of your moral and ethical questions
+          </Typography>
+          <Typography
+            component="p"
+            variant="h6"
             sx={{ mt: 2, textAlign: "center" }}
           >
             We Know All...
