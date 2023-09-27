e7be23e9a8811dc22054b1aa6c2f915d3ef3bd98
bwyma1 <brockdwyma@gmail.com>
2023-09-26 21:18:39 -0500

    chatbot page frontend complete

--- frontend/src/Components/Router.jsx
+++ frontend/src/Components/Router.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Homepage from "../Views/Homepage";
 import ResponsiveNavbar from "./Navbar";
 import AIChatBot from "../Views/AIChatBot";
+import AboutUs from "../Views/AboutUs";
+import HowToUse from "../Views/HowToUse";
 
 export const Router = () => (
   <BrowserRouter>
@@ -10,6 +12,8 @@ export const Router = () => (
       <Route path="/" element={<Homepage />} />
       <Route path="homepage" element={<Homepage />} />
       <Route path="aichatbot" element={<AIChatBot />} />
+      <Route path="aboutus" element={<AboutUs />} />
+      <Route path="howtouse" element={<HowToUse />} />
     </Routes>
   </BrowserRouter>
 );
--- frontend/src/Views/AIChatBot.jsx
+++ frontend/src/Views/AIChatBot.jsx
@@ -1,15 +1,51 @@
 import React from "react";
-import Button from "@mui/material/Button";
-import CssBaseline from "@mui/material/CssBaseline";
+import PropTypes from "prop-types";
 import TextField from "@mui/material/TextField";
-import Link from "@mui/material/Link";
-import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { useTheme } from "@emotion/react";
+import { AppBar, Tab, Tabs } from "@mui/material";
 
-const theme = createTheme();
+const darkTheme = createTheme({
+  // palette: {
+  //   mode: 'dark',
+  // },
+});
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`full-width-tabpanel-${index}`}
+      aria-labelledby={`full-width-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `full-width-tab-${index}`,
+    "aria-controls": `full-width-tabpanel-${index}`,
+  };
+}
 
 export default function AIChatBot() {
   //submitting the form and logging in if correct credentials
@@ -20,10 +56,20 @@ export default function AIChatBot() {
     (async () => {})();
   };
 
+  const theme = useTheme();
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  const handleChangeIndex = (index) => {
+    setValue(index);
+  };
+
   return (
-    <ThemeProvider theme={theme}>
-      <Container component="main" maxWidth="xs">
-        <CssBaseline />
+    <ThemeProvider theme={darkTheme}>
+      <Container maxWidth="sm">
         <Box
           sx={{
             marginTop: 8,
@@ -32,62 +78,53 @@ export default function AIChatBot() {
             alignItems: "center",
           }}
         >
-          <Typography component="h1" variant="h5">
-            Sign in
+          <Typography component="h1" variant="h5" centered>
+            -Chat Bot-
           </Typography>
-          <Box
-            component="form"
-            onSubmit={handleSubmit}
-            noValidate
-            sx={{ mt: 1 }}
-          >
+        </Box>
+        <Box sx={{ bgcolor: "background.paper" }}>
+          <Box component="form" onSubmit={handleSubmit} noValidate>
             <TextField
-              sx={{
-                margin: "normal",
-              }}
+              sx={{ mt: 3, mb: 2 }}
               required
               fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              type="email"
-              autoComplete="email"
+              id="question"
+              label="Enter Your Question Here"
+              name="question"
+              type="question"
+              autoComplete="question"
               autoFocus
             />
-            <TextField
-              sx={{
-                margin: "normal",
-                mt: 3,
-              }}
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-            />
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-            >
-              Sign In
-            </Button>
-            <Grid container>
-              <Grid item xs>
-                <Link href="/passwordreset" variant="body2">
-                  Forgot password?
-                </Link>
-              </Grid>
-              <Grid item>
-                <Link href="/signup" variant="body2">
-                  {"Don't have an account? Sign Up"}
-                </Link>
-              </Grid>
-            </Grid>
           </Box>
+          <AppBar position="static">
+            <Tabs
+              value={value}
+              onChange={handleChange}
+              indicatorColor="secondary"
+              textColor="inherit"
+              variant="fullWidth"
+              aria-label="full width tabs example"
+            >
+              <Tab label="Against" {...a11yProps(0)} />
+              <Tab label="Neutral" {...a11yProps(1)} />
+              <Tab label="For" {...a11yProps(2)} />
+            </Tabs>
+          </AppBar>
+          <view
+            axis={theme.direction === "rtl" ? "x-reverse" : "x"}
+            index={value}
+            onChangeIndex={handleChangeIndex}
+          >
+            <TabPanel value={value} index={0} dir={theme.direction}>
+              Against answer to question
+            </TabPanel>
+            <TabPanel value={value} index={1} dir={theme.direction}>
+              Neutral answer to question
+            </TabPanel>
+            <TabPanel value={value} index={2} dir={theme.direction}>
+              For answer to question
+            </TabPanel>
+          </view>
         </Box>
       </Container>
     </ThemeProvider>
